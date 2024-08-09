aec88b3756fd1d977b220723a5d82fdb0032272c
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2024-08-09 20:10:09 +0530

    Grider: Deck Shuffle

--- grider/deck/src/main.rs
+++ grider/deck/src/main.rs
@@ -1,3 +1,5 @@
+use rand::{thread_rng, seq::SliceRandom};
+
 #[derive(Debug)]
 struct Deck{
     cards: Vec<String>,
@@ -20,13 +22,14 @@ impl Deck {
         Deck{cards}
     }
 
-    fn shuffle(&self) {
-        
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
     }
 }
 
 fn main() {
-    let deck = Deck::new();
+    let mut deck = Deck::new();
     deck.shuffle();
     println!("Deck: {:#?}", deck);
 }
