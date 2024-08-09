5cb0752c921181fe12e7afd9960ba7e26cddfaaf
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2024-08-10 00:14:59 +0530

    Grider: Media (More enum variants, and Catalog struct)

--- grider/media/src/main.rs
+++ grider/media/src/main.rs
@@ -3,6 +3,8 @@ enum Media {
     Book{ title: String, author: String},
     Movie { title: String, director:String},
     Audiobook {title: String},
+    Podcast (u32),
+    Placeholder
 }
 
 impl Media {
@@ -28,8 +30,28 @@ impl Media {
             },
             Media::Audiobook { title } => {
                 format!("Audiobook: {}", title)
+            },
+            Media::Podcast(episode_number) => {
+                format!("Podcast: {}", episode_number)
+            },
+            Media::Placeholder => {
+                format!("Placeholder:")
             }
         }
+    }
+}
+
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>
+}
+impl Catalog {
+    fn new() -> Self {
+        Catalog {items: vec![]}
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media);
     }
 }
 
@@ -54,4 +76,15 @@ fn main() {
         author: String::from("Mishra"),
     };
     println!("{:#?}",book.description());
+
+    let podcast = Media::Podcast(99);
+    let placeholder = Media:: Placeholder;
+
+    let mut catalog = Catalog::new();
+    catalog.add(audiobook);
+    catalog.add(movie);
+    catalog.add(book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+    println!("{:#?}", catalog);
 }
