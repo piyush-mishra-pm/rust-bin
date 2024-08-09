d9bc697abd734da4098d4a620e75801d8019c2c9
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2024-08-09 23:59:23 +0530

    Grider: Media (Enum with pattern matching)

--- grider/media/src/main.rs
+++ grider/media/src/main.rs
@@ -7,14 +7,28 @@ enum Media {
 
 impl Media {
     fn description(&self) -> String {
-        if let Media::Book{title, author} = self {
-            format!("Book: {} {}", title, author)
-        }else if let Media::Movie{title, director} = self  {
-            format!("Movie: {} {}", title, director)
-        }else if let Media::Audiobook { title } = self  {
-            format!("Audiobook: {}", title)
-        }else{
-            format!("Unknown media!")
+        // VERBOSE SELF TYPE CHECK
+        // if let Media::Book{title, author} = self {
+        //     format!("Book: {} {}", title, author)
+        // }else if let Media::Movie{title, director} = self  {
+        //     format!("Movie: {} {}", title, director)
+        // }else if let Media::Audiobook { title } = self  {
+        //     format!("Audiobook: {}", title)
+        // }else{
+        //     format!("Unknown media!")
+        // }
+
+        // PATTERN MATCHING:
+        match self {
+            Media::Book { title, author } => {
+                format!("Book: {} {}", title, author)
+            },
+            Media::Movie { title, director } => {
+                format!("Movie: {} {}", title, director)
+            },
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
         }
     }
 }
@@ -34,4 +48,10 @@ fn main() {
         director: String::from("Stephen Hawking"),
     };
     println!("{:#?}",movie.description());
+
+    let book = Media::Book { 
+        title: String::from("Panchtantra!"), 
+        author: String::from("Mishra"),
+    };
+    println!("{:#?}",book.description());
 }
