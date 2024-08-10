fb121f2f483cc439cee860bde9267e80114fc43f
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2024-08-10 11:44:19 +0530

    Grider:Logs: Try operator, vs Unwrap, expect vs Match, if,let

--- grider/logs/src/main.rs
+++ grider/logs/src/main.rs
@@ -1,10 +1,55 @@
 use std::{fs, io::Error};
 
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+    return results;
+}
+
+fn main() -> Result<(), Error> {
+    // validation_and_strings_test_code();
+
+    // Method 1: When can meanigfully deal with error.
+    // match fs::read_to_string("logs.txt")  {
+    //     Ok(parsed_text) => {
+    //         println!("✅ Parsed successfully. Length {} chars.", parsed_text.len());
+    //         let error_logs = extract_errors(parsed_text.as_str());
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => println!("✅ Written to file"),
+    //             Err(reason_write_failed) => {
+    //                 println!("❌ Error in writing errors.txt file: {}", reason_write_failed);
+    //             }
+    //         }
+    //     },
+    //     Err(why_failed) => {
+    //         println!("❌ Failed to read: {}", why_failed);
+    //     }
+    // } ;
+
+    // Method 2: unwrap, expect:
+    // let text = fs::read_to_string("logs.txt")
+    //         .expect("❌ Failed to read");
+    // let error_logs = extract_errors(text.as_str());
+    // fs::write("errors.txt", error_logs.join("\n")).expect("❌ Error in writing errors.txt file");
+
+    // Method 3: When not method 1. Dont have a workaround for the error.
+    let text = fs::read_to_string("sdlogs.txt")?;
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n"))?;
+    Ok(())
+}
+
 fn string_test(a:String, b:&String, c:&str){
 
 }
 
-fn main() {
+fn validation_and_strings_test_code(){
     string_test("Red".to_string(), &String::from("Red"), String::from("Red").as_str());
     string_test("Red".to_string(), &String::from("Red"), &String::from("Red"));
 
@@ -25,15 +70,6 @@ fn main() {
         Ok(..)=> println!("✅ Email valid"),
         Err(err)=>println!("❌ Error: {:#?}", err)
     }
-
-    match fs::read_to_string("logs.txt")  {
-        Ok(parsed_text) => {
-            println!("✅ Parsed successfully. Length {} chars.", parsed_text.len())
-        },
-        Err(why_failed) => {
-            println!("❌ Failed to read: {}", why_failed)
-        }
-    } ;
 }
 
 fn divide(a:f64, b: f64) -> Result<f64, Error> {
