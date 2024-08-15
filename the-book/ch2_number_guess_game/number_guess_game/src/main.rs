fca6b2ea2874e4e6fcca16571b8202c46cc254cc
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2024-08-15 15:57:12 +0530

    The-Book: NumberGame: Refactor

--- the-book/ch2_number_guess_game/number_guess_game/src/main.rs
+++ the-book/ch2_number_guess_game/number_guess_game/src/main.rs
@@ -4,37 +4,26 @@ static LOWER_RANGE: u32 = 1;
 static HIGHER_RANGE: u32 = 10;
 
 #[derive(Debug)]
-struct Game{
+struct Game {
     number: u32,
-    attempts: u32,
+    attempts: usize,
     guesses: Vec<u32>,
 }
-
-impl Game {
-    fn new(number:u32, attempts:u32, guesses:Vec<u32>)->Self{
-        Self{
-            number,
-            attempts,
-            guesses,
-        }
-    }
-}
-
+// todo: Think if Vec<> in below code can be used with just refs, and if there is unneccessary clonin happening as of now.
 fn main() {
     println!(
         "Welcome (🙏) to\nNumber Guessing Game!\n🎉\n\tTry guessing the number between {} & {}",
         LOWER_RANGE, HIGHER_RANGE
     );
     let mut game_history: Vec<Game> = vec![];
-    let game_number = 0;
     loop {
+        let game_number = game_history.len() + 1;
         println!("Starting Game #{}:", game_number);
         let secret_number = rand::thread_rng().gen_range(LOWER_RANGE..=HIGHER_RANGE);
-        let mut attempts: u32 = 0;
         let mut guesses: Vec<u32> = vec![];
         loop {
             // Increment attempt number:
-            attempts += 1;
+            let attempts = guesses.len() + 1;
 
             // Receive guessed input from User:
             let mut guessed = String::new();
@@ -59,10 +48,14 @@ fn main() {
                 std::cmp::Ordering::Greater => println!("😲 Too big. Try smaller!"),
                 std::cmp::Ordering::Equal => {
                     println!("🎯 Bingo! You guessed {secret_number} in {attempts} attempts!");
-                    game_history.push(Game::new(secret_number, attempts, guesses));
+                    game_history.push(Game {
+                        number: secret_number,
+                        attempts,
+                        guesses,
+                    });
                     println!("\nGame History: * * * * * *\n\tnumber\tattempts\tguesses");
                     game_history.iter().for_each(|game| {
-                        println!("\t{}\t{}\t{:?}",game.number, game.attempts, game.guesses);
+                        println!("\t{}\t{}\t{:?}", game.number, game.attempts, game.guesses);
                     });
 
                     break;
